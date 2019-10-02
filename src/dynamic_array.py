3cc957c323375831f7efc177d1098bb86c7676f5
Jonathan Dorety <kyullog@gmail.com>
2019-10-01 19:22:57 -0700

    Sketch out insert method

--- src/dynamic_array.py
+++ src/dynamic_array.py
@@ -7,15 +7,24 @@ class DynamicArray:
 
     def append(self, value):
         if self.count >= self.capacity:
-            #TODO go and resize
+            # TODO go and resize
             print("ERROR: already full")
             return value
-            
+
         self.storage[self.count] = value
         self.count += 1
 
     def insert(self, index, value):
-        pass
+        if self.count >= self.capacity:
+            # TODO go and resize
+            print("ERROR: already full")
+            return value
+
+        for i in range(self.count, index, -1):
+            self.storage[i] = self.storage[i - 1]
+        self.storage[index] = value
+        self.count += 1
+        return value
 
     def resize(self):
         pass
